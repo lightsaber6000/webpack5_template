b5d5c77d3e202f1ba4025d48f3972d45979de745
Domestos002 <absorber000@gmail.com>
2022-03-13 15:49:50 +0600

    fix files loop

--- build/utils.js
+++ build/utils.js
@@ -1,25 +1,23 @@
 const glob = require('glob');
 
-// имя файла должно совпадать с именем папки
 exports.getEntries = function (context, extension) {
-  const entries = {};
-  const folders = glob.sync(`${context}/*`);
-
   if (context[context.length - 1] !== '/') {
     context += '/';
   }
 
-  extension = Array.isArray(extension) ? extension.map(el => `.${el}`) : `.${extension}` ;
+  extension = Array.isArray(extension) ? extension : [extension];
 
-  folders.forEach((folder) => {
-    const name = folder.replace(context, '');
-    entries[`${name}/${name}`] = Array.isArray(extension)
-      ? extension.map(el => glob.sync(`${context}**/*${el}`)).flat()
-      : glob.sync(`${context}**/*${extension}`)[0];
+  const match = extension.length > 1 ? `.{${extension.join(',')}}` : `.${extension[0]}`;
+  const files = glob.sync(`${context}**/*${match}`);
+  const entries = {};
+
+  files.forEach((file) => {
+    const match = extension.length > 1 ? extension.join('|') : extension[0];
+    const re = new RegExp(`\.(${match})`, 'i');
+    const prop = file.replace(context, '').replace(re, '');
+    if (entries[prop] == null) entries[prop] = [];
+    entries[prop] = [...entries[prop], file];
   });
-
-  // console.log('entries:');
-  // console.log(entries);
 
   return entries;
 };
